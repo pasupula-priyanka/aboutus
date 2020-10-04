3dfb9464c453274a15eb63f2ad5ac40c09eb7325
pasupula priyanka <pasupulapriyanka123@gmail.com>
2020-10-04 16:16:58 +0530

    initial commit

---
+++ about.js
@@ -0,0 +1,54 @@
+import React from 'react'
+
+const Aboutus = ()=>{
+    return(
+        <div>
+
+                 
+                     <div className="heading">
+                          <h1>About us</h1>
+                          
+                    <p className="text">
+                    <b>L</b>orem ipsum dolor sit amet, consectetur adipiscing elit,
+                     sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                     Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                     sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                     sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                     Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                     sunt in culpa qui officia deserunt mollit anim id est laborum.orem ipsum dolor sit amet, consectetur adipiscing elit,
+                     sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                     Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                     sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </p>
+                 </div>
+                 <div className="team-section">
+                     <div>
+                      <h1>Meet Our Founders</h1>
+                          <div className="ps1">
+                          <img src="https://images.unsplash.com/photo-1601652221547-bd12baeeefbb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="" />
+                          
+                          <img src="https://images.unsplash.com/photo-1601652221547-bd12baeeefbb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt=""/>  
+                          </div>
+                          </div>
+                          
+                              <h1>Meet Our Team</h1>
+                              <div >
+                               <img className="ps2" src="https://images.unsplash.com/photo-1601652221547-bd12baeeefbb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="" />
+                               <img className="ps2" src="https://images.unsplash.com/photo-1601652221547-bd12baeeefbb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="" />
+                               <img className="ps2" src="https://images.unsplash.com/photo-1601652221547-bd12baeeefbb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt=""/>  
+                               <img className="ps2" src="https://images.unsplash.com/photo-1601652221547-bd12baeeefbb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt=""/> 
+                               <img className="ps2" src="https://images.unsplash.com/photo-1601652221547-bd12baeeefbb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="" />
+                               <img className="ps2" src="https://images.unsplash.com/photo-1601652221547-bd12baeeefbb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="" />
+                               <img className="ps2" src="https://images.unsplash.com/photo-1601652221547-bd12baeeefbb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="" />
+                               <img className="ps2" src="https://images.unsplash.com/photo-1601652221547-bd12baeeefbb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="" />
+
+                             
+                         </div>  
+                 </div>
+
+                 </div>
+        
+    )
+}
+
+export default Aboutus;
